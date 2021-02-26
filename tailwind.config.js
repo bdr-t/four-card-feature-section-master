0d7426aa65e926548f57e89e1c3b2e62bf79e97b
Bader <btabeche23@gmail.com>
2021-02-26 16:38:48 +0100

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+    },
+    colors: {
+      red: 'hsl(0, 78%, 62%)',
+      cyan: 'hsl(180, 62%, 55%)',
+      orange: 'hsl(34, 97%, 64%)',
+      blue: 'hsl(212, 86%, 64%)',
+      veryDarkBlue: 'hsl(234, 12%, 34%)',
+      grayishBlue: 'hsl(229, 6%, 66%)',
+      lightGray: 'hsl(0, 0%, 98%)',
+      white: 'hsl(0, 0%, 100%)',
+    },
+    screens: {
+      'md': {'max': '1024px'},
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
